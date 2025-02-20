17f08699b3557703ca9ac8a9c1b55ac2e5d844a6
성현규 <hyungyugod@naver.com>
2025-02-20 23:24:46 +0900

    [level 0] Title: 분수의 덧셈, Time: 158.54 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 117.33 ms
+메모리: 10.4 MB, 시간: 158.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 20일 16:41:20
+2025년 02월 20일 23:24:44
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,26 +1,17 @@
-def solution(numer1, denom1, numer2, denom2):
+def min_min(a, b):      
     s = []
     t = []
-    j = []
-    k = []
 
-    for i in range(1, denom1+1):
-        if denom1 % i == 0:     
+    for i in range(1, a+1):
+        if a % i == 0:     
             s.append(i)
 
     for x in s:
-        if denom2 % x == 0:
+        if b % x == 0:
             t.append(x)
+    return t[-1]
 
-    d = denom1 * denom2 // t[-1]                        
-    f = (d // denom1) * numer1 + (d // denom2) * numer2     
-    
-    for i in range(1,d+1):
-        if d % i == 0:     
-            j.append(i)
-
-    for x in j:
-        if f % x == 0:
-            k.append(x)
-
-    return [f / k[-1],d / k[-1]]
+def solution(numer1, denom1, numer2, denom2):    
+    d = denom1 * denom2 // min_min(denom1,denom2)
+    f = (d // denom1) * numer1 + (d // denom2) * numer2                          
+    return [f / min_min(d,f),d / min_min(d,f)]
