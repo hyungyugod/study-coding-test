1070cda493eb6deed3533a94548fe834366187cb
성현규 <hyungyugod@naver.com>
2025-03-04 01:12:00 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,21 @@
+def solution(num):
+    answer = 0
+    if num == 1:
+        answer = 0
+    else:
+        while num > 1:
+            if answer == 500:
+                answer = -1
+                break  
+                
+            elif num % 2 == 0:
+                num = num // 2
+                answer += 1
+                
+            elif num % 2 == 1:
+                num = 3 * num + 1
+                answer += 1
+                       
+    if num != 1:
+        answer = -1
+    return answer
