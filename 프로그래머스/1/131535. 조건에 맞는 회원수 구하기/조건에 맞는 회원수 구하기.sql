91896e3443c269073eaaf57e138591e83d63f7f4
성현규 <hyungyugod@naver.com>
2025-04-14 10:20:01 +0900

    [level 1] Title: 조건에 맞는 회원수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
+++ 프로그래머스/1/131535. 조건에 맞는 회원수 구하기/조건에 맞는 회원수 구하기.sql
@@ -1,3 +1,3 @@
-SELECT count(USER_ID) as USERS 
+SELECT COUNT(*) as USERS 
     from USER_INFO 
-    where JOINED like '2021%' and (AGE between 20 and 29);
+    where TO_CHAR(JOINED, 'YYYY') = '2021' AND (AGE between 20 and 29);
