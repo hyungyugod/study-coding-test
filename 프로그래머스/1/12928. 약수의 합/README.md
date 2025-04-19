5ee5b13ee13be856c7f022ea826ec71163f593fe
성현규 <hyungyugod@naver.com>
2025-04-19 21:18:08 +0900

    [level 1] Title: 약수의 합, Time: 2.11 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/README.md
+++ 프로그래머스/1/12928. 약수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 1] 약수의 합 - 12928 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12928?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.16 ms
+메모리: 71.5 MB, 시간: 2.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 03일 12:54:24
+2025년 04월 19일 21:18:05
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,25 @@
+import java.security.KeyStore.Entry;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.Scanner;
+class Solution {
+    public int solution(int n) {
+    return IntStream.rangeClosed(1, n)
+                    .filter(i -> n % i == 0)
+                    .sum();
+}
+}
