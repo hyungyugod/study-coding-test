12e1eb2c9785d191bc39e1bf0796e31beb44f986
성현규 <hyungyugod@naver.com>
2025-05-10 12:29:31 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 5.16 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.03 ms
+메모리: 83.8 MB, 시간: 5.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 03일 17:20:14
+2025년 05월 10일 12:29:28
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,31 @@
+import java.security.KeyStore.Entry;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+import java.util.Scanner;
+class Solution {
+    public long solution(long n) {
+    String sn = Long.toString(n);
+    String on = Arrays.stream(sn.split(""))
+                    .sorted(Comparator.reverseOrder())
+                    .collect(Collectors.joining(""));
+    return Long.parseLong(on);
+}
+}
