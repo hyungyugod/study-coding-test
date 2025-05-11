298114bd960aef5d26b6f834d5b689b2fe22f8e3
성현규 <hyungyugod@naver.com>
2025-05-11 14:45:01 +0900

    [level 1] Title: 하샤드 수, Time: 1.72 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/README.md
+++ 프로그래머스/1/12947. 하샤드 수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 하샤드 수 - 12947 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12947) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12947?language=java) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.03 ms
+메모리: 93.2 MB, 시간: 1.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 04일 01:11:17
+2025년 05월 11일 14:44:59
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,30 @@
+import java.security.KeyStore.Entry;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+import java.util.Scanner;
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int y = Arrays.stream(Integer.toString(x).split(""))
+                    .mapToInt(Integer::parseInt).sum();
+        return x % y == 0;
+}
+}
