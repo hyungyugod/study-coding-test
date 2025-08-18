6b3ffa0f826a39f95aeec643ab28e835f65d7ed5
성현규 <hyungyugod@naver.com>
2025-08-18 15:42:48 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.go
@@ -0,0 +1,9 @@
+func solution(price int) int {
+    if price >= 500000 {
+        return int(float32(price) * 0.8)
+    } else if price >= 300000 {
+        return int(float32(price) * 0.9)
+    } else if price >= 100000 {
+        return int(float32(price) * 0.95)
+    } else {return price}
+}
